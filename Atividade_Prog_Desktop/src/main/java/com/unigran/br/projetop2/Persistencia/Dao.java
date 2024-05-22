cef54dd5719078d53d4037efc11e5333c1eeddf1
EduPanage <panagee3@gmail.com>
2024-05-21 20:09:39 -0400

    Criação da Classe Dao (Persistencia) e modificação/adequação do Pom.xml

---
+++ Atividade_Prog_Desktop/src/main/java/com/unigran/br/projetop2/Persistencia/Dao.java
@@ -0,0 +1,68 @@
+package br.unigran.Persistencia;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Dao {
+    private static Dao instancia;
+    private final EntityManagerFactory emf;
+    private final EntityManager em;
+
+    private Dao() {
+        emf = Persistence.createEntityManagerFactory("br.unigran_ProjetoP2_jar_1.0-SNAPSHOTPU");
+        em = emf.createEntityManager();
+    }
+
+    public static Dao getInstace() {
+        if (instancia == null) {
+            synchronized (Dao.class) {
+                if (instancia == null) {
+                    instancia = new Dao();
+                }
+            }
+        }
+        return instancia;
+    }
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public Object salvar(Object o) {
+        em.getTransaction().begin();
+        em.persist(o);
+        em.getTransaction().commit();
+        return o;
+    }
+
+    public Object atualizar(Object o) {
+        em.getTransaction().begin();
+        Object retorno = em.merge(o);
+        em.getTransaction().commit();
+        return retorno;
+    }
+
+    public void remover(Object o) {
+        em.getTransaction().begin();
+        em.remove(em.contains(o) ? o : em.merge(o));
+        em.getTransaction().commit();
+    }
+
+    public <T> T buscarPorId(Class<T> clazz, Object id) {
+        return em.find(clazz, id);
+    }
+
+    public <T> List<T> listarTodos(Class<T> clazz) {
+        return em.createQuery("SELECT o FROM " + clazz.getSimpleName() + " o", clazz).getResultList();
+    }
+
+    public void fechar() {
+        if (em != null && em.isOpen()) {
+            em.close();
+        }
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+}
