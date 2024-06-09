9a6a4e0a4d48c9ae45b07996079826587d44ba5c
Pedro <joao@joao.com>
2024-06-08 22:56:21 -0300

    Início das Telas - Branch Alpha 0.8.0
    Telas
    
    Adição no comportamento de classes.
    Criação das telas de login, cadastro e administrador.
    
    By João Pedro Marques Martins

---
+++ Atividade_Prog_Desktop/src/main/java/com/unigran/br/projetop2/controllers/CadastroImplementacao.java
@@ -0,0 +1,55 @@
+package com.unigran.br.projetop2.controllers;
+
+import com.unigran.br.projetop2.Dao.LoginDao;
+import com.unigran.br.projetop2.model.Login;
+
+public class CadastroImplementacao {
+    //OBS prática: somente o usuário ADIMINISTRADOR poderá criar cadastros. Na Tela, ele poderá
+    //clicar em uma setinha que vai aparecer as permissões de usuários possiveis que ele desejar, e ele vai selecionar uma
+    //para fazer aquele cadastro específico, passando o usuario, o login, e a permissão
+    public static boolean efetuarCadastro(String user, String senha, Integer permissao) throws Exception { //ATENÇÃO! Classe tela deve ter tratamento caso retorne false!
+        if (senha != null && !senha.isEmpty() && user != null && !user.isEmpty() && permissao!=null) {
+            Login login = new Login();
+
+            login.setSenha(senha);
+            login.setLogin(user);
+            login.setPermissao(permissao);
+
+            LoginDao loginController = new LoginDao();
+            loginController.salvar(login);
+
+            return true;   //Cadastrado com Sucesso (Mostrar tela)
+        }else {
+            System.err.print("Dados insuficientes");
+            return false; //Deve aparecer um erro na Tela
+        }
+    }
+
+    public static Integer verificaPermissao(String permissao){
+        Integer permissaoInteger;
+
+        switch (permissao){
+            case "Administrador":
+                permissaoInteger=1;
+                break;
+
+            case "Dentista":
+                permissaoInteger=2;
+                break;
+
+            case "Gerente":
+                permissaoInteger=3;
+                break;
+
+            case "Recepcionista":
+                permissaoInteger=4;
+                break;
+
+            default:
+                permissaoInteger=100;
+                break;
+        }
+
+        return permissaoInteger;
+    }
+}
--- Atividade_Prog_Desktop/src/main/java/com/unigran/br/projetop2/controllers/LoginImplementacao.java
+++ Atividade_Prog_Desktop/src/main/java/com/unigran/br/projetop2/controllers/LoginImplementacao.java
@@ -2,33 +2,12 @@ package com.unigran.br.projetop2.controllers;
 
 import com.unigran.br.projetop2.Dao.LoginDao;
 import com.unigran.br.projetop2.model.Login;
+import com.unigran.br.projetop2.views.TelasAdm.TelaAdministrador;
 
 import java.util.List;
 
-public class LoginCadastroImplementacao {
-
-    //OBS prática: somente o usuário ADIMINISTRADOR poderá criar cadastros. Na Tela, ele poderá
-    //clicar em uma setinha que vai aparecer as permissões de usuários possiveis que ele desejar, e ele vai selecionar uma
-    //para fazer aquele cadastro específico, passando o usuario, o login, e a permissão
-    public static boolean efetuarCadastro(String user, String senha, Integer permissao) throws Exception { //ATENÇÃO! Classe tela deve ter tratamento caso retorne false!
-        if (senha != null && !senha.isEmpty() && user != null && !user.isEmpty() && permissao!=null) {
-            Login login = new Login();
-
-            login.setSenha(senha);
-            login.setLogin(user);
-            login.setPermissao(permissao);
-
-            LoginDao loginController = new LoginDao();
-            loginController.salvar(login);
-
-            return true;   //Cadastrado com Sucesso (Mostrar tela)
-        }else {
-            System.err.print("Dados insuficientes");
-            return false; //Deve aparecer um erro na Tela
-        }
-    }
-
-    public static void validaLogin(String user,String senha) {
+public class LoginImplementacao {
+    public static boolean validaLogin(String user,String senha) {
         boolean sucesso;
         sucesso = false;
         if (senha != null && !senha.isEmpty() && user != null && !user.isEmpty()) {
@@ -52,7 +31,8 @@ public class LoginCadastroImplementacao {
 
                         switch(permissao) {
                             case 1:
-                                // telaPrincipalPermissão1AtendenteSeila(user).setVisible(true);
+                                System.out.print("ALOOOOOOOOOOOOOOO");
+                                new TelaAdministrador().setVisible(true);
                                 break;
 
                             case 2:
@@ -73,13 +53,16 @@ public class LoginCadastroImplementacao {
         }else{
             //Dados Invalidos // Tratar TELA
             System.err.print("Dados invalidos");
+            return false;
         }
 
         if (sucesso==false)
         {
             System.err.print("Usuario ou senha inválidos");
+            return false;
         }else {
             System.err.print("Logado Com sucesso2");
+            return true;
         }
     }
 }
